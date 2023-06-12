22a30caf9a18645214973bd05631afc42f3dadb2
Giyazkartanov <gizzidarkshark@gmail.com>
2023-06-12 22:09:51 +0600

    main.py changes

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+git init 
+git add .
+git status
+git commit -m "First Commit"
+git remote add origin <HTTPS link.git> - ремут тоже пишется единожды
+git branch
+git push -u origin master
+
+"""
